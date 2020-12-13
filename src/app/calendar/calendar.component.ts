e5c3f35499fe1e1d4d44ecec3a42107e1b0750d4
Kristina <kristina.maro4kina@yandex.ru>
2020-12-14 00:18:55 +0300

    Calendar plus +-total money

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -7,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calendar.component.css']
 })
 export class CalendarComponent implements OnInit {
-  public i = 0; // начальная точка отсчета месяца
+  public i = 0; // начальная точка отсчета месяцев
+  totalMoney = 123;
   constructor() {
   }
   ngOnInit(): void {
+    document.getElementById('variable').innerHTML = String(this.totalMoney);
     this.calendar(this.i);
   }
   calendar(i: number): void {
@@ -25,7 +27,6 @@ export class CalendarComponent implements OnInit {
       month = month + 12;
       year -= 1;
     }
-    alert(month);
     const firstDay = new Date(year, month, 1);
     const firstWday = firstDay.getDay();
     const nextMonth = new Date(year, month + 1, 1).getMonth();
@@ -36,14 +37,14 @@ export class CalendarComponent implements OnInit {
     dayTable.id = 'Calendar';
     mainSection.appendChild(dayTable);
     dayTable.style.fontFamily = 'BulletJournal2, \'Bullet Journal\', cursive';
-    dayTable.style.textAlign = 'center';
+   // dayTable.style.textAlign = 'center';
     dayTable.style.border = 'solid 2px rgb(89,89,77)';
     dayTable.style.borderSpacing = '0px';
     dayTable.style.width = '100%';
     dayTable.style.height = '100%';
     dayTable.style.background = '#d4cabe';
     dayTable.innerHTML = '<tr><td colspan=7 rowspan=1 id=\'month\'>' + n[month] + '</td></tr>' +
-      '<tr id=\"weekday\"><td>Sunday</td><td>Monday</td><td>Tuesday</td><td>Wednesday</td><td>Thursday</td><td>Friday</td><td>Saturday</td></tr>' +
+      '<tr id=\"weekday\"><td width="10%">Sunday</td><td width="16%">Monday</td><td width="16%">Tuesday</td><td width="16%">Wednesday</td><td width="16%">Thursday</td><td width="16%">Friday</td><td width="10%">Saturday</td></tr>' +
       '<tr><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>' +
       '<tr><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>' +
       '<tr><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>' +
@@ -65,11 +66,27 @@ export class CalendarComponent implements OnInit {
     for (let j = 1; j <= 7; j++){
       tdD[j].style.borderBottom = 'solid 2px rgb(89,89,77)';
       tdD[j].style.borderTop = 'solid 2px rgb(89,89,77)';
+      tdD[j].style.textAlign = 'center';
+    }
+    for (let j = 8; j <= 49; j++){
+      tdD[j].style.paddingLeft = '30px';
+      tdD[j].style.fontSize = '30px';
     }
     for (let k = 0; k < lastDate + firstWday; k++) {
       if (k >= firstWday) {
-        tdD[8 + k].innerHTML = String(k + 1 - firstWday);
+        tdD[8 + k].innerHTML = '<button class="day">' + String(k + 1 - firstWday) + '</button>';
       }
+    }
+    // кнопки
+    const dDay = document.getElementsByClassName('day') as HTMLCollectionOf<HTMLElement>;;
+    for (let j = 0; j <= 41; j++){
+      dDay[j].style.width = '100%';
+      dDay[j].style.height = '100%';
+      dDay[j].style.backgroundColor = '#d4cabe';
+      dDay[j].style.border = 'none';
+      dDay[j].style.cursor = 'crosshair';
+      dDay[j].style.textAlign = 'left';
+      dDay[j].style.outline = 'none';
     }
   }
   ClickButton(i: number): void { // параметры +1 и -1
@@ -77,6 +94,17 @@ export class CalendarComponent implements OnInit {
     mainSection.innerHTML = '';
     this.i += i;
     this.calendar(this.i);
+  }
+
+  EditButton(): void {
+    const mainSection = document.getElementById('edit');
+    const editForm = document.createElement('form');
+    mainSection.appendChild(editForm);
+    editForm.innerHTML = '<input id="editValue"></input><button (click)="SaveButton()">Save</button>';
+  }
+
+  SaveButton(): void{
+    // ????????
   }
 }
 
